c1c362be48c23b34a1fec2ecdded4a19425da203
Michael Iedema <michael.iedema@rangenetworks.com>
2013-11-20 13:38:07 +0000

    - increase retries in libsqliteodbc to 100 - rename package to indicate range custom version
    git-svn-id: http://wush.net/svn/range/software/commercial/libsqliteodbc/trunk@6873 19bc5d8c-e614-43d4-8b26-e1612bc8e597

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+VERSION=0.995
+
+sayAndDo () {
+	echo $@
+	eval $@
+	if [ $? -ne 0 ]
+	then
+		echo "ERROR: command failed!"
+		exit 1
+	fi
+}
+
+installIfMissing () {
+	dpkg -s $@ > /dev/null
+	if [ $? -ne 0 ]; then
+		echo " - oops, missing $@, installing"
+		sudo apt-get install $@
+	else
+		echo " - $@ ok"
+	fi
+	echo
+}
+
+if [ ! -f sqliteodbc-$VERSION.tar.gz ]
+then
+	sayAndDo wget http://www.ch-werner.de/sqliteodbc/sqliteodbc-$VERSION.tar.gz
+fi
+
+if [ -d sqliteodbc-$VERSION ]
+then
+	sayAndDo rm -rf sqliteodbc-$VERSION
+fi
+
+sayAndDo tar zxf sqliteodbc-$VERSION.tar.gz
+sayAndDo cp debian/* sqliteodbc-$VERSION/debian/
+sayAndDo patch -p0 < add_retries.patch
+sayAndDo cd sqliteodbc-$VERSION
+sayAndDo dpkg-buildpackage
